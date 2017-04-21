b802b9640076cd5466bf39dba9e06e5c21227c53
Aurelien van Delft <avdelft@ulb.ac.be>
2017-04-21 14:53:10 +0200

    [enh] prog struct

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pygame, sys
-pygame.init()
-size = width, height = 960, 720
-screen = pygame.display.set_mode(size)
-screen.fill((255,218,185))
-pygame.display.update()
+
+def window_init():
+	pygame.init()
+	size = width, height = 960, 720
+	screen = pygame.display.set_mode(size)
+	screen.fill((255,218,185))
+	pygame.display.update()
+
+def get_canvas():
+	pass
+
 def main():
+	window_init()
 	while 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT: sys.exit()
